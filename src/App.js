7b5f4b3653cd14144df51c1f90634342bc321fea
yw850 <wangyongze.ze@gmail.com>
2020-03-26 22:35:53 +1100

    useSelector

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from "react";
-import { connect } from "react-redux";
+// import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 import { increment, decrement } from "./redux/index";
 
 const App = props => {
+  const count = useSelector(state => state);
   return (
     <div>
-      <h1>{props.count}</h1>
+      <h1>{count}</h1>
       <button onClick={props.increment}>+</button>
       <button onClick={props.decrement}>-</button>
     </div>
   );
 };
 /* What parts of state do you want? , What actions to dispatch? */
-const mapStateToProps = globalState => ({ count: globalState });
-const mapDispatchToProps = {
-  increment,
-  decrement
-};
+// const mapStateToProps = globalState => ({ count: globalState });
+// const mapDispatchToProps = {
+//   increment,
+//   decrement
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
