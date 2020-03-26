4e4a4b531a23745d83ccbc91e692fc550758ac6b
yw850 <wangyongze.ze@gmail.com>
2020-03-26 21:37:11 +1100

    setup

--- src/redux/index.js
+++ src/redux/index.js
@@ -7,11 +7,43 @@ import redux, { createStore } from "redux";
  */
 
 // 1. Create action creators for having the count "increment" and "decrement"
+export const increment = () => {
+  return {
+    type: "INCREMENT"
+  };
+};
+
+export const decrement = () => {
+  return {
+    type: "DECREMENT"
+  };
+};
 
 // 2. Create a reducer to handle your increment and decrement actions
+const reducer = (count = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT": {
+      return {
+        count: count + 1
+      };
+    }
+    case "DECREMENT": {
+      return {
+        count: count - 1
+      };
+    }
+    default:
+      return count;
+  }
+};
 
 // 3. Create a new Redux store
+const store = redux.createStore(reducer);
 
 // 4. Set up the subscribe function so we can more easily see the changes to the Redux state as they happen
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
 // 5. Export the store as a default export
+export default store;
